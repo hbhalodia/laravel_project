baab97038a62e1bd0c07991bcbc5282112d940e0
Hit Bhalodia <hitkumar.bhalodia105373@marwadiuniversity.ac.in>
2021-01-06 14:53:39 +0530

    Minor Fixes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -11,7 +11,11 @@ use App\Http\Requests\TodoCreateRequest;
 
 class TodoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(){
         $todos = auth()->user()->todos()->orderBy('completed')->get(); //at sql level
         //buy using collection -> $todos = auth()->user()->todos->sortBy('completed')->get();
@@ -21,13 +25,24 @@ class TodoController extends Controller
         return view('todos.index',compact('todos'));
     }
 
-    public function show(Todo $todo){
-        return view('todos.show',compact('todo'));
-    }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create(){
         return view('todos.create');
     }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    
     public function store(TodoCreateRequest $request){
 
         /* not good below validation we need different logic for that */
@@ -65,15 +80,51 @@ class TodoController extends Controller
         return redirect(route('todo.index'))->with('message','To do created succesfully');
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo){
+        return view('todos.show',compact('todo'));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit(Todo $todo){
         //$todo = Todo::find($id);  if directly passing id
         //if passing the model from routeas route model binding
         return view('todos.edittodolist',compact('todo'));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(TodoCreateRequest $request , Todo $todo){
         $todo -> update(['title' => $request->title , 'description' => $request->description]);
         return redirect(route('todo.index'))->with('message','Todo Updated'); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo){
+        $todo->steps->each->delete();
+        $todo -> delete();
+        return redirect()->back()->with('message','To do Deleted!');
     }
 
     public function complete(Todo $todo){
@@ -84,9 +135,6 @@ class TodoController extends Controller
         $todo->update(['completed' => false]);
         return redirect()->back()->with('message','To do marked as incompleted!');
     }
-    public function destroy(Todo $todo){
-        $todo->steps->each->delete();
-        $todo -> delete();
-        return redirect()->back()->with('message','To do Deleted!');
-    }
+
+    
 }
